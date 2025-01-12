6e4f7d762569db894b01129b58862d45f91ebc44
Sadat388 <sadathossain388@gmail.com>
2025-01-12 06:08:58 +0600

    developed button workings

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Display from "./components/Display";
 import styles from "./App.module.css";
 import ButtonsContainer from "./components/ButtonsContainer";
+import { useState } from "react";
 
 function App() {
+  const [initialDisplay, setDisplay] = useState("");
+  const onButtonClick = (event) => console.log(event);
+
   return (
     <div className={styles.calculator}>
-      <Display></Display>
-      <ButtonsContainer></ButtonsContainer>
+      <Display displayValue={initialDisplay}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
     </div>
   );
 }
--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -1,58 +1,126 @@
 import styles from "./ButtonsContainer.module.css";
 
-const ButtonsContainer = () => {
+const ButtonsContainer = ({ onButtonClick }) => {
   return (
     <>
       <div className={styles.buttonsContainer}>
-        <button className={styles.button}>Rad</button>
-        <button className={styles.button}>Deg</button>
-        <button className={styles.button}>x!</button>
-        <button className={styles.button}>(</button>
-        <button className={styles.button}>)</button>
-        <button className={styles.button}>%</button>
-        <button className={styles.button}>AC</button>
+        <button className={styles.button} onClick={() => onButtonClick("Rad")}>
+          Rad
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("Deg")}>
+          Deg
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("x!")}>
+          x!
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("(")}>
+          (
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick(")")}>
+          )
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("%")}>
+          %
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("AC")}>
+          AC
+        </button>
       </div>
 
       <div className={styles.buttonsContainer}>
-        <button className={styles.button}>Inv</button>
-        <button className={styles.button}>sin</button>
-        <button className={styles.button}>ln</button>
-        <button className={styles.button}>7</button>
-        <button className={styles.button}>8</button>
-        <button className={styles.button}>9</button>
-        <button className={styles.button}>÷</button>
+        <button className={styles.button} onClick={() => onButtonClick("Inv")}>
+          Inv
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("sin")}>
+          sin
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("ln")}>
+          ln
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("7")}>
+          7
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("8")}>
+          8
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("9")}>
+          9
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("÷")}>
+          ÷
+        </button>
       </div>
 
       <div className={styles.buttonsContainer}>
-        <button className={styles.button}>π</button>
-        <button className={styles.button}>cos</button>
-        <button className={styles.button}>log</button>
-        <button className={styles.button}>4</button>
-        <button className={styles.button}>5</button>
-        <button className={styles.button}>6</button>
-        <button className={styles.button}>×</button>
+        <button className={styles.button} onClick={() => onButtonClick("π")}>
+          π
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("cos")}>
+          cos
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("log")}>
+          log
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("4")}>
+          4
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("5")}>
+          5
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("6")}>
+          6
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("×")}>
+          ×
+        </button>
       </div>
 
       <div className={styles.buttonsContainer}>
-        <button className={styles.button}>e</button>
-        <button className={styles.button}>tan</button>
-        <button className={styles.button}>√</button>
-        <button className={styles.button}>1</button>
-        <button className={styles.button}>2</button>
-        <button className={styles.button}>3</button>
-        <button className={styles.button}>-</button>
+        <button className={styles.button} onClick={() => onButtonClick("e")}>
+          e
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("tan")}>
+          tan
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("√")}>
+          √
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("1")}>
+          1
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("2")}>
+          2
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("3")}>
+          3
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("-")}>
+          -
+        </button>
       </div>
 
       <div className={styles.buttonsContainer}>
-        <button className={styles.button}>Ans</button>
-        <button className={styles.button}>EXP</button>
-        <button className={styles.button}>
+        <button className={styles.button} onClick={() => onButtonClick("Ans")}>
+          Ans
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("EXP")}>
+          EXP
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("x^y")}>
           x<sup>y</sup>
         </button>
-        <button className={styles.button}>0</button>
-        <button className={styles.button}>.</button>
-        <button className={styles.button}>=</button>
-        <button className={styles.button}>+</button>
+        <button className={styles.button} onClick={() => onButtonClick("0")}>
+          0
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick(".")}>
+          .
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("=")}>
+          =
+        </button>
+        <button className={styles.button} onClick={() => onButtonClick("+")}>
+          +
+        </button>
       </div>
     </>
   );
